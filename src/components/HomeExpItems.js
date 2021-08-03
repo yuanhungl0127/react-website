a7abdbad84b69872678db177b1e4b88f178b5573
YuanHungLin <viclin@linyuanhongde-MacBook-Pro.local>
2021-08-03 15:29:04 +1000

    USM demo link

--- src/components/HomeExpItems.js
+++ src/components/HomeExpItems.js
@@ -7,7 +7,7 @@ import todos from "../images/todos.png";
 import ums from "../images/UMS.png";
 
 export const HomeExpItems = [
-    {title: "User Management System", src: ums, text:["React.js / Hooks / Redux", "Node.js / Express", "MongoDB Cloud", "Restful APIs"], github: "https://github.com/Viclin0127/express-user-management-system", demo: {isDemo: true, url: ""}},
+    {title: "User Management System", src: ums, text:["React.js / Hooks / Redux", "Node.js / Express", "MongoDB Cloud", "Restful APIs"], github: "https://github.com/Viclin0127/express-user-management-system", demo: {isDemo: true, url: "https://yuan-ums.netlify.app"}},
     {title: "Unsplash Pictures Searching App", src: pics, text:["React.js / Hooks", "JavaScript ES6 and above", "axios"], github: "https://github.com/Viclin0127/unsplash-pictures-searching", demo: {isDemo: true, url: "https://yuan-pics-app.netlify.app"}},
     {title: "Youtube Videos Searching App", src: videos, text:["React.js / Hooks", "JavaScript ES6 and above", "axios"], github: "https://github.com/Viclin0127/youtube-videos", demo: {isDemo: true, url: "https://yuan-videos-app.netlify.app"}},
     {title: "Todo List", src: todos, text:["React.js / Hooks", "JavaScript ES6 and above"], github: "https://github.com/Viclin0127/react-todo-app", demo: {isDemo: true, url: "https://yuan-todo-list.netlify.app"}},
